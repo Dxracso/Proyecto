f5be63cd9c82bf0918ca0371648a238296871c9b
Oscar Andres Hernandez Ariza <Oscar Andres Hernandez Ariza>
2016-02-09 17:54:00 -0500

    Termine clase propietario

---
+++ InformationBranch/Desarrollo/V 0.2 Proyecto/ProyectoDAO/src/edu/co/sena/modelo/dto/Propietario.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.modelo.dto;
+
+/**
+ *
+ * @author Dxracso
+ */
+public class Propietario {
+    
+    private String equipoIdEquipo;
+    private String cuentaNumeroDocumento;
+    private String cuentaTipoDocumento;
+
+    public String getEquipoIdEquipo() {
+        return equipoIdEquipo;
+    }
+
+    public void setEquipoIdEquipo(String equipoIdEquipo) {
+        this.equipoIdEquipo = equipoIdEquipo;
+    }
+
+    public String getCuentaNumeroDocumento() {
+        return cuentaNumeroDocumento;
+    }
+
+    public void setCuentaNumeroDocumento(String cuentaNumeroDocumento) {
+        this.cuentaNumeroDocumento = cuentaNumeroDocumento;
+    }
+
+    public String getCuentaTipoDocumento() {
+        return cuentaTipoDocumento;
+    }
+
+    public void setCuentaTipoDocumento(String cuentaTipoDocumento) {
+        this.cuentaTipoDocumento = cuentaTipoDocumento;
+    }
+    
+    
+}
