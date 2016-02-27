a3a10d481f44d8259c552ca786ec0fe2b42c8dcf
Usuario <Usuario@SENA>
2016-02-27 11:51:16 -0500

    Finalizacion pruebas resourceManager

--- InformationBranch/Desarrollo/V 0.2 Proyecto/ProyectoDAO/test/edu/co/sena/modelo/dao/mysql/ResourceManagerTest.java
+++ InformationBranch/Desarrollo/V 0.2 Proyecto/ProyectoDAO/test/edu/co/sena/modelo/dao/mysql/ResourceManagerTest.java
@@ -60,6 +60,35 @@ public class ResourceManagerTest {
      //  ResourceManager.close(rs);
         
     }
+    @org.junit.Test
+    public void testRsClose()throws SQLException{
+    Connection con = null;
+    PreparedStatement st =null;
+    ResultSet rs=null;
+    String sql="select*from catalogo;";    
+    try {
+        con = ResourceManager.conectar();
+        st = con.prepareStatement(sql);
+            rs = st.executeQuery();
+            while (rs.next()) {            
+                System.out.println("Id Catalogo: "+rs.getInt(1));
+                System.out.println("nombre :"+rs.getString(2));
+                System.out.println(" desciroc: "+rs.getString(3));
+        }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } finally{
+        if (rs!=null) {
+            ResourceManager.close(rs);
+        }
+        if (st!=null) {
+            ResourceManager.close(st);
+        }
+        if (con!=null) {
+            ResourceManager.close(con);
+        }
+    }
+            }
 
     
 }
